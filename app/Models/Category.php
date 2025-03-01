a81c9856f3ef6f076b6b13d7be71e5b246c053c3
PeYoT <putraananta08@gmail.com>
2025-03-01 23:45:07 +0700

    add category crud

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    /**
+     * Relasi ke produk (One-to-Many)
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'category_id');
+    }
+
+    /**
+     * Scope untuk mencari kategori berdasarkan nama
+     */
+    public function scopeSearch($query, $keyword)
+    {
+        return $query->where('name', 'like', '%' . $keyword . '%');
+    }
 }
