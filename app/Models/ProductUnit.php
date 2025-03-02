53ace496d5b6cec9872ed79fff01e355f55fe2ba
PeYoT <putraananta08@gmail.com>
2025-03-03 00:26:33 +0700

    add productUnit database

---
+++ app/Models/ProductUnit.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductUnit extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'product_id',
+        'barcode',
+        'selling_price',
+        'conversion_rate',
+    ];
+
+    /**
+     * Relasi ke Product
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
