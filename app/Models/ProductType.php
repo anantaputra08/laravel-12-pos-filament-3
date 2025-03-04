05f960e533754a5a7d36e28edaabac3461c87cda
PeYoT <putraananta08@gmail.com>
2025-03-04 15:30:19 +0700

    edit models

--- app/Models/ProductType.php
+++ app/Models/ProductType.php
@@ -2,9 +2,29 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductType extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['name'];
+
+    /**
+     * Relasi ke produk (One-to-Many)
+     */
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'type_id');
+    }
+
+    /**
+     * Scope untuk mencari tipe produk berdasarkan nama
+     */
+    public function scopeSearch($query, $keyword)
+    {
+        return $query->where('name', 'like', '%' . $keyword . '%');
+    }
 }
