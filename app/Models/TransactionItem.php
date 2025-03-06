dc9fd8f87e24d92bfbb492740d32278fbd44bb84
PeYoT <putraananta08@gmail.com>
2025-03-07 00:10:00 +0700

    add transaction_items

---
+++ app/Models/TransactionItem.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TransactionItem extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'transaction_id',
+        'product_id',
+        'product_price',
+        'qty',
+        'total_price',
+        'timestamp'
+    ];
+
+    public $timestamps = false;
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transaction_id');
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+}
---
+++ database/migrations/2025_03_06_075631_create_transaction_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transaction_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaction_id')->constrained('transactions');
+            $table->foreignId('product_id')->constrained('products');
+            $table->decimal('product_price', 15, 2)->default(0);
+            $table->integer('qty')->default(1);
+            $table->decimal('total_price', 15, 2)->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transaction_items');
+    }
+};
