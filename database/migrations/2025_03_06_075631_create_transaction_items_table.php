757dd7a9e3deb580eaeeefa79f468dd39f0dd862
PeYoT <putraananta08@gmail.com>
2025-03-11 23:50:21 +0700

    add coloums

--- database/migrations/2025_03_06_075631_create_transaction_items_table.php
+++ database/migrations/2025_03_06_075631_create_transaction_items_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->foreignId('transaction_id')->constrained('transactions');
             $table->foreignId('product_id')->constrained('products');
+            $table->foreignId('product_unit_id')->nullable();
+            $table->boolean('is_base_unit')->default(true);
             $table->decimal('product_price', 15, 2)->default(0);
             $table->integer('qty')->default(1);
             $table->decimal('total_price', 15, 2)->default(0);
