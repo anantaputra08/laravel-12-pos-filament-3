2f2396cc513583e66ca9250a6cc06a00a1e1ee22
PeYoT <putraananta08@gmail.com>
2025-03-12 22:01:32 +0700

    add seeders product type

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,5 +22,12 @@ class DatabaseSeeder extends Seeder
             'email' => 'admin@example.com',
             'password' => bcrypt('1234'),
         ]);
+
+        $productTypes = [
+            ['name' => 'Default'],
+            ['name' => 'Multi Satuan'],
+        ];
+
+        DB::table('product_types')->insert($productTypes);
     }
 }
